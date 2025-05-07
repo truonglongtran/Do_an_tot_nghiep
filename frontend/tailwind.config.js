c9ed4e22ce70a926a199262e34aa17cfd641858f
LongTran-Pionero <long.tran_it@pionero.io>
2025-05-07 15:30:47 +0700

    ....

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [ 
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx,vue,html}",
-  ],
-  theme: {
-    extend: {},
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [vue()],
+  css: {
+    postcss: './postcss.config.js',
   },
-  plugins: [],
-}
+})
